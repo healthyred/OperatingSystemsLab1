46948ecf78f05c4f5d1c7947de221bb77678d2c3
Jian H. Lu <19jhl3@lohani.cs.williams.edu>
2019-02-06 22:47:27 -0500

    ver

--- inverter.cc
+++ inverter.cc
@@ -5,8 +5,13 @@
 #include <string>
 #include <iostream>    // Needed to perform IO operations
 #include <vector>
+#include <map>
+#include <set>
+
 using namespace std;
 
+
+map<string, set<int> > invertedIndex;
 //input >> noskipws >> c; /*parsing the input*/
 
 vector<string> readFiletoVector(string filename){
@@ -20,12 +25,27 @@ vector<string> readFiletoVector(string filename){
   return lines;
 }
 
+vector<string> readFiletoVectorbyWord(string filename){
+  //reads in the words and sets as vector
+
+  
+}
+
 void displayVector(const vector<string> v){
 
   for(int i(0); i != v.size(); ++i){
     cout << "\n" << v[i];
   }
+
+  cout << "\n";
+}
+
+void addWordsfromVectorList(const vector<string> v){
+
+  for(int i(0); i != v.size(); i++){
     
+  }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -40,7 +60,7 @@ int main(int argc, char *argv[]) {
   vector<string> texts = readFiletoVector(listFilename);
   displayVector(texts);
   
-  // map<string, set<int> > invertedIndex;
+  
   
   // cout << "hello, world" << endl;  // Say Hello
   return 0;          
